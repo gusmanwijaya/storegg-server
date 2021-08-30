94f06eb7aa769df0a1e3a0d4302e53744fa0bfc2
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-08-30 22:32:46 +0700

    CRUD menggunakan ODM Mongoose

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,9 +1,75 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", {
-        title: "Express",
+      const category = await Category.find();
+      res.render("admin/category/view_category", {
+        category,
       });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      const category = await Category({ name });
+      await category.save();
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({
+        _id: id,
+      });
+
+      res.render("admin/category/edit", { category });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      await Category.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        {
+          name,
+        }
+      );
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      await Category.findOneAndDelete({
+        _id: id,
+      });
+
+      res.redirect("/category");
     } catch (error) {
       console.log(error);
     }
