0fe45f766878072bfbc8fcb5515c5e648676a82e
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-02 17:56:25 +0700

    API Category

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -36,6 +37,18 @@ module.exports = {
       res.status(200).json({
         data: voucher,
       });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: error.message || "Terjadi kesalahan pada server",
+      });
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+
+      res.status(200).json({ data: category });
     } catch (error) {
       res.status(500).json({
         status: "error",
