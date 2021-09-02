4002a481b4762257f75b87b25e7a81e5869d43f2
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-02 22:34:07 +0700

    install cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
 
+const cors = require("cors");
+
 const categoryRouter = require("./app/category/router");
 const dashboardRouter = require("./app/dashboard/router");
 const nominalRouter = require("./app/nominal/router");
@@ -22,6 +24,8 @@ const authRouter = require("./app/auth/router");
 const app = express();
 
 const URL = `/api/v1`;
+
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
