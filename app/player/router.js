a4254dbcc207fb6ec99b46dae58847d83693d982
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-02 21:00:23 +0700

    API history transaction with filter data

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+} = require("./controller");
 
 const { isLoginPlayer } = require("../middleware/auth");
 
@@ -9,4 +15,6 @@ router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+
 module.exports = router;
