b906d4f9829b0868563e3ed61758fc3bf935342e
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-02 21:37:47 +0700

    API Dashboard

--- app/player/router.js
+++ app/player/router.js
@@ -8,6 +8,7 @@ const {
   checkout,
   history,
   historyDetail,
+  dashboard,
 } = require("./controller");
 
 const { isLoginPlayer } = require("../middleware/auth");
@@ -18,5 +19,6 @@ router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
 router.get("/history", isLoginPlayer, history);
 router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/dashboard", isLoginPlayer, dashboard);
 
 module.exports = router;
